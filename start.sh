f614ca31f2808a5cd3926e23dd31c291e07c11cf
Zafer Akbiyik <zaferakbiyik@icloud.com>
2022-06-10 23:14:59 +0200

    t

--- start.sh
+++ start.sh
@@ -4,6 +4,4 @@ killall screen
 rm -rf zafer
 wget https://raw.githubusercontent.com/zaferakbiyik/Mainn/main/danicim.sh
 chmod 777 danicim.sh
-chmod 777 a+w danicim.sh
-chmod 777 +x danicim.sh
 screen -dm -S "danicim" ./danicim.sh
